97e86e34eb79b512c90526f8f0f5a20b22efe868
Lok Lok <yellowhappy831@gmail.com>
2026-03-09 09:05:54 +0800

    save

--- src/components/3d/AIMaterial/useAICode.ts
+++ src/components/3d/AIMaterial/useAICode.ts
@@ -1,12 +1,7 @@
 import { create } from "zustand";
-
-export const useAICode = create<any>((set, get) => {
-  return {
-    //
-    prompt: `const material = new THREE.MeshPhysicalNodeMaterial({})
+const tempplate = `const material = new THREE.MeshPhysicalNodeMaterial({})
 
 material.transmissionNode = float(1.0);
-material.thicknessNode = float(1.5).add(sin(time.add(positionLocal.length()))); 
 
 material.roughnessNode = float(0.1);
 material.metalnessNode = float(0.2);
@@ -17,14 +12,21 @@ const noise = vec3(
     cos(positionLocal.y.mul(10).add(sin(time).add(time))),
 ).normalize();
 
+material.thicknessNode = float(1.5).add(sin(time.add(positionLocal.length()))); 
+
 material.positionNode = positionLocal.add(
     noise
 );
 
-// make it blue and water-ish
+// make it blue and water-ish and wavy based on world space and make use of the noise above
 material.colorNode = vec4(1.0);
 
-return material;
+return material;`;
+
+export const useAICode = create<any>((set, get) => {
+  return {
+    //
+    prompt: `${tempplate}
     `.trim(),
     isRunning: false,
     ctxSize: 256_000,
@@ -32,55 +34,7 @@ return material;
     draft: `
     `.trim(),
     code: `
-const material = new THREE.MeshPhysicalNodeMaterial({})
-
-material.transmissionNode = float(1.0);
-material.thicknessNode = float(1.5).add(sin(time.add(positionLocal.length()))); 
-
-material.roughnessNode = float(0.1);
-material.metalnessNode = float(0.2);
-
-const noise = vec3(
-    sin(positionLocal.y.mul(10).add(sin(time).add(time))),
-    positionLocal.y.mul(2),
-    cos(positionLocal.y.mul(10).add(sin(time).add(time))),
-).normalize();
-
-material.positionNode = positionLocal.add(
-    noise
-);
-
-// make it blue and water-ish
-material.colorNode = vec4(1.0);
-
-return material;
+${tempplate}
 `.trim(),
   };
 });
-
-/*
-const material = new THREE.MeshPhysicalNodeMaterial({})
-
-
-material.transmissionNode = float(1.0);
-material.thicknessNode = float(1.5).add(sin(time.add(positionLocal.length()))); 
-
-material.roughnessNode = float(0.1);
-material.metalnessNode = float(0.2);
-
-const noise = vec3(
-        sin(positionLocal.y.mul(10).add(sin(time).add(time))),
-        positionLocal.y.mul(2),
-        cos(positionLocal.y.mul(10).add(sin(time).add(time))),
-    );
-
-material.positionNode = positionLocal.add(
-    noise
-);
-
-// make it rainbow colored aligned to the swirl
-// todo: change this line:
-material.colorNode = color( new Color(  'rgba(255, 255, 255, 1)' ) )
-
-return material;
-*/
